36acdccada6527837f613beab8f7e0120b131831
projhovet <caiobrasilp@gmail.com>
2023-03-06 13:18:24 -0300

    edicao de arquivos do deposito

--- painel-adm/deposito/deposito.php
+++ painel-adm/deposito/deposito.php
@@ -29,9 +29,42 @@
                     ?>
                     <tr>
                         <td><?=$dados["id"]?></td>
-                        <td>nome teste</td>
+
+                        <!--pega o nome do produto a partir do id cadastrado-->
+                        <?php
+                            $id_produto_nome = $dados["nome_produtoID"];
+                            $sql_select = "SELECT * FROM insumos where id=$id_produto_nome";
+                            $result = mysqli_query($conexao,$sql_select) or die("Erro ao executar a consulta! " . mysqli_error($conexao));
+                            while($nome_produto = mysqli_fetch_assoc($result)){
+                                
+                        ?>
+                        <td><?=$nome_produto["nome"]?></td>
+                        <?php
+                            }
+                        ?>
+
                         <td><?=$dados["quantidade"]?></td>
-                        <td>Tipo teste</td>
+
+                        <!--pega o nome do produto a partir do id cadastrado-->
+                        <?php
+                            $id_produto_tipo = $dados["tipo_produtoID"];
+                            $sql_select = "SELECT * FROM insumos where id=$id_produto_tipo";
+                            $result = mysqli_query($conexao,$sql_select) or die("Erro ao executar a consulta! " . mysqli_error($conexao));
+                            while($tipo_produto = mysqli_fetch_assoc($result)){
+                                
+                        ?>
+                        <td>
+                            <?php
+                            if($tipo_produto["insumo_tipo"] == 1 ){
+                                echo "Medicamento";
+                            }else{
+                                echo "Materiais de procedimentos médicos";
+                            }
+                            ?>
+                        </td>
+                        <?php
+                            }
+                        ?>
                         <td><?=$dados["setor"]?></td>
                         <td><?=$dados["validade"]?></td>
                         <td>
@@ -44,7 +77,7 @@
                                 $vencimentoMES = $intervalo->m;
                             
                                 if (($vencimentoMES <= 1) and ($vencimentoDIAS > 0)) {
-                                    echo "o produto está vencido em " . $vencimentoDIAS . "dia(s)";
+                                    echo "o produto está vencido em " . $vencimentoDIAS . " dia(s)";
                                 }else{
                                     echo "Diferença de " . $vencimentoDIAS . " dia(s) ";
                                     echo "e " . $vencimentoMES . " mese(s) ";
