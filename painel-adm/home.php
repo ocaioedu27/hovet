0ecd2b59785d595e429a36cbc4e56d354b0b92a6
projhovet <caiobrasilp@gmail.com>
2023-03-13 14:42:05 -0300

    ajustes no painel principal

--- painel-adm/home.php
+++ painel-adm/home.php
@@ -2,7 +2,9 @@
     <div class="group_cards">
         <div class="cards">
             <div class="insumosDeposito">
-                <h2>Depósito</h2>
+                <div class="top_card">
+                    <h2>Depósito</h2>
+                </div>
                 <h4>Insumos
                     <span class="icon">
                         <ion-icon name="file-tray-full-outline"></ion-icon>
@@ -30,24 +32,46 @@
             </div>
         </div>
         <div class="cards">
-            <h4>Card Movimentações</h4>
+            <div class="top_card">
+                <h2>Movimentacoes</h2>
+            </div>
             <div class="movimentacoes">
                 <div>
-                    <h5>Inserções</h5>
+                    <h4>Inserções</h5>
+                    <h5>7</h5>
                 </div>
                 <div>
-                    <h5>Retiradas</h5>
+                    <h4>Retiradas</h4>
+                    <h5>2</h5>
                 </div>
                 <div>
-                    <h5>Doações</h5>
+                    <h4>Doações</h4>
+                    <h5>3</h5>
                 </div>
                 <div>
-                    <h5>Permutas</h5>
+                    <h4>Permutas</h4>
+                    <h5>4</h5>
                 </div>
             </div>
         </div>
         <div class="cards">
-            <p>Card Relatórios</p>
+            <div class="top_card">
+                <h2>Relatórios</h2>
+            </div>
+            <div class="relatorios">
+                <div class="relatorioInserts">
+                    <a href="#">Visualizar Relatório de Cadastro de Insumos</a>
+                </div>
+                <div class="relatorioRetiradas">
+                    <a href="#">Visualizar Relatório de Retirada de Insumos</a>
+                </div>
+                <div class="relatorioDoacoes">
+                    <a href="#">Visualizar Relatório de Doações de Insumos</a>
+                </div>
+                <div class="relatorioPermutas">
+                    <a href="#">Visualizar Relatório de Permutas de Insumos</a>
+                </div>
+            </div>
         </div>
     </div>
 </div>
